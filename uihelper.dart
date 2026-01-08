0d3ac81fecc6999daed37c3b93ed400a5b1ffb32
Nandini Dnyaneshwar Vaijawade <163897805+NandiniV-20@users.noreply.github.com>
2026-01-08 18:37:01 +0530

    UiHelper for helping the UI screen

---
+++ uihelper.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+class UiHelper{
+  static CustomTextButton({required String text, required VoidCallback callback}){
+    return TextButton(onPressed: (){
+      callback();
+    },
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 12,color: Color(0XFF3797EF)),));
+  }
+
+  static CustomTextField({required TextEditingController controller, required String text, required bool tohide}){
+    return Container(
+      height: 50,
+      width: 343,
+      decoration: BoxDecoration(
+        color: Color(0XFF121212),
+        border: Border.all(color: Colors.white24),
+        borderRadius: BorderRadius.circular(5)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15),
+        child: TextField(
+          controller: controller,
+          obscureText: tohide,
+          decoration: InputDecoration(
+              hintText: text,
+              hintStyle: TextStyle(fontSize: 14,fontWeight: FontWeight.normal, color: Color(0XFFFFFFFF)),border: InputBorder.none
+
+          ),
+        ),
+      )
+    );
+  }
+
+  static CustomImage({required String imgurl}){
+    return Image.asset("assets/images/$imgurl");
+  }
+
+  static CustomButton({
+    required VoidCallback callback, required String buttonname
+}){
+    return SizedBox(
+      height: 45,
+      width: 343,
+      child: ElevatedButton(onPressed: (){
+        callback();
+      },style: ElevatedButton.styleFrom(backgroundColor: Colors.pinkAccent, shape: RoundedRectangleBorder()), child: Center(
+        child: Text(buttonname,style: TextStyle(fontSize: 14, color: Colors.white),),
+
+      )),
+    );
+  }
+
+}
